f91092fc4a9a8aaa326ac70087e041618451eea5
nathanieljwise <nathaniel.wise@gmail.com>
2024-03-05 11:32:19 -0500

    Bare bones landing page

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, abort
 from api_utils import *
 
-app = Flask(__name__)
+def ParseVehicles(currentVehicles: list[Vehicle]) -> list[dict[str, str]]:
+    tableInfo = []
+    for vehicle in currentVehicles:
+        thisId = vehicle.id
+        thisRouteName = getRouteName(vehicle.routeId)
+        tableInfo.append({'id': thisId, 'name': thisRouteName})
+    return tableInfo
 
+rtVehicles = getRealTimeVehiclePositions()
+# showAllVehicles(rtVehicles)
+vehiclesTable = ParseVehicles(rtVehicles)
+# rtTrips = getRealTimeTripUpdates()
+# showAllTrips(rtTrips)
+
+
+
+app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', vehicles=vehiclesTable)
 
 if __name__ == "__main__":
     app.run(debug=True)
