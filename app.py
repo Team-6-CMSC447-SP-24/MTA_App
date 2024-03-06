81b41b9df0ad9f7425b03f617daf71db321499a7
nathanieljwise <nathaniel.wise@gmail.com>
2024-03-06 14:32:35 -0500

    add tripupdate page with upcoming stops; bare bones

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, abort
 from api_utils import *
 
-def ParseVehicles(currentVehicles: list[Vehicle]) -> list[dict[str, str]]:
-    tableInfo = []
-    for vehicle in currentVehicles:
-        thisId = vehicle.id
-        thisRouteName = getRouteName(vehicle.routeId)
-        tableInfo.append({'id': thisId, 'name': thisRouteName})
-    return tableInfo
-
-rtVehicles = getRealTimeVehiclePositions()
-# showAllVehicles(rtVehicles)
-vehiclesTable = ParseVehicles(rtVehicles)
-# rtTrips = getRealTimeTripUpdates()
-# showAllTrips(rtTrips)
-
-
 
 app = Flask(__name__)
 @app.route('/')
 def index():
+    rtVehicles = getRealTimeVehiclePositions()
+    vehiclesTable = ParseVehicles(rtVehicles)
     return render_template('index.html', vehicles=vehiclesTable)
+
+@app.route('/<int:id>/tripupdate', methods=('GET', 'POST'))
+def tripupdate(id):
+    rtTrips = getRealTimeTripUpdates()
+    thisTrip = None
+    theseStops = []
+    for trip in rtTrips:
+        if int(trip.tripId) == int(id):
+            thisTrip = trip
+    theseStops= ParseStops(thisTrip)
+    
+    return render_template('tripupdate.html', stops=theseStops)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
+    # showAllVehicles(rtVehicles)
+    #showAllTrips(rtTrips)
