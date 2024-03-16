bed1b9c832a2d0c3c0c135cec319c8e47f8e064a
BostonCreamDonut <42999297+BostonCreamDonut@users.noreply.github.com>
2024-03-16 17:48:13 -0400

    finished unit test for user story#1

--- test_KnowRoute.py
+++ test_KnowRoute.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template, request, url_for, flash, redirect, abort
 from api_utils import *
 from credentials import google_key
 import unittest
+from mta_classes import Vehicle, Trip
 
 #1 As a rider, I want to know what route a bus is driving so I get on the right vehicle for my intended destination.
-#2 As a commuter, I want to be able to view real-time information for MTA buses so that I can plan my journey more effectively.
-#3 As a user, I want to be able to view the full schedule for a specific route so that I can plan my journey in advance.
-#4 As a user, I want to be able to search for specific bus or train routes so that I can quickly find the information I need.
 
 class TestKnowRoute(unittest.TestCase):
 
@@ -19,10 +16,33 @@ class TestKnowRoute(unittest.TestCase):
         print('teardownClass')
 
     def setUp(self):
+        self.rtVehicles = getRealTimeVehiclePositions()
+        self.vehiclesTable = ParseVehicles(self.rtVehicles)
         pass
     
     def tearDown(self):
         pass
 
-    def test_X(self):
-        pass
+    def test_getVehichles(self): #test getRealTimeVehicleUpdates
+        self.assertIsNot(len(self.rtVehicles), 0)
+
+    def test_getRoute(self): #test getRouteName
+        route = getRouteName(11684)
+        self.assertTrue("Downtown" in route)    
+
+    def test_allRoutes(self): #test all routes can be found in parse, test ParseVehicles
+        routes = []
+        check = 0
+        for vehicle in self.rtVehicles:
+            routes.append(getRouteName(vehicle.routeId))
+        for route in routes:
+            check = 0
+            for parse in self.vehiclesTable:
+                if route in parse['name']:
+                    check = 1
+            if check == 0:
+                break
+        self.assertTrue(check, 1)
+            
+if __name__ == '__main__':
+    unittest.main()
